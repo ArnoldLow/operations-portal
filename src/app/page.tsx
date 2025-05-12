09ab20625af47abc07199a16daed8f4eaca0956e
Arnold.L <arnoldjllow@gmail.com>
2025-05-12 20:20:52 +0100

    adjusted gap x on homepage grid to match design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout/index";
 export default async function Home() {
   return (
     <Layout>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-16 px-6 md:px-28 pb-9">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-24 px-6 md:px-28 pb-9">
         {/* Meetings Section */}
         <section>
           <h2 className="text-2xl text-gray-600 mb-8">Meetings</h2>

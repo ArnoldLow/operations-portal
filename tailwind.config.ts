e0b0d989000af93c0ddb16f1d618acc907641c14
Arnold.L <arnoldjllow@gmail.com>
2025-05-12 01:54:27 +0100

    added partial color theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,24 @@ const config: Config = {
       fontFamily: {
         sans: ['"Helvetica Neue"', "Helvetica", "Arial", "sans-serif"],
       },
+      colors: {
+        stone: "#EEEEEE",
+        beige: {
+          light: "#F3F0E7",
+        },
+        gray: {
+          standard: "#DDDDDD",
+          dark: "#CCCCCC",
+          light: "#EEF1F4",
+        },
+        navy: {
+          gray: "#545F71",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          shell: "#F9F9F9",
+        },
+      },
     },
   },
   plugins: [],
